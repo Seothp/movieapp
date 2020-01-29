68e4f5547b5792ed54635da770770b520f12c2c0
Nikolay Geldyev <nikoejento@gmail.com>
2020-01-29 11:19:11 +0300

    add loading images and titles of filse via TMD API

--- src/components/movies-list/movies-list.jsx
+++ src/components/movies-list/movies-list.jsx
@@ -1,7 +1,24 @@
 import React, { useState, useEffect } from 'react';
-import { TMD_URL, DISCOVER_PATH } from '../../constants'
-import { Box } from '@material-ui/core';
+import { TMD_URL, DISCOVER_PATH, TMD_API_KEY } from '../../constants'
+import { Card, CardMedia, Grid, Typography } from '@material-ui/core';
+import { makeStyles  } from '@material-ui/core/styles';
 
+
+const useStyles = makeStyles({
+    card: {
+        boxSizing: 'border-box',
+        width: 280,
+        padding: 24,
+        flexGrow: 0,
+        flexShrink: 0,
+        marginBottom: 24,
+    },
+    media: {
+        width: 200,
+        height: 300,
+        marginBottom: 8,
+    },
+});
 
 export const MoviesList = () => {
     const [ lists, setLists ] = useState([]);
@@ -10,21 +27,36 @@ export const MoviesList = () => {
         fetchMovies()
     }, [])
     const fetchMovies = () => {
-        fetch(`${TMD_URL}${DISCOVER_PATH}movie?api_key=5dea54ae7c9d9964e72e2ee872e49402`)
+        fetch(`${TMD_URL}${DISCOVER_PATH}movie?${TMD_API_KEY}`)
             .then(res => res.json())
             .then(result => {
                 console.log(result);
                 setLists(result.results);
             })
     }
-    
+    const classes = useStyles()
     return (
-        <Box className='movie-list'>
+        <Grid 
+        className='movie-list'
+        container
+        direction="row"
+        justify="space-around"
+        >
             {lists.map((item) => (
-                <div key={item.id}>
+                <Card 
+                key={item.id}
+                className={classes.card}>
+                    {item.poster_path && 
+                        <CardMedia 
+                        image={`https://image.tmdb.org/t/p/original${item.poster_path}`} 
+                        title="poster"
+                        className={classes.media}/>
+                    }
+                    <Typography variant='h5' component='h3'>
                     {item.title}
-                </div>
+                    </Typography>
+                </Card>
             ))}
-        </Box>
+        </Grid>
     )
 }
--- src/constants.js
+++ src/constants.js
@@ -2,3 +2,4 @@ export const TMD_URL = 'https://api.themoviedb.org/3/';
 export const MOVIE_PATH = 'movie/';
 export const TV_SHOW_PATH = 'tv/';
 export const DISCOVER_PATH = 'discover/';
+export const TMD_API_KEY = 'api_key=5dea54ae7c9d9964e72e2ee872e49402'
