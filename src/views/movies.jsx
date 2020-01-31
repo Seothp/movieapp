a4a3451ca1168396846083557fc3729121d6827d
Nikolay Geldyev <nikoejento@gmail.com>
2020-01-31 10:26:29 +0300

    add routing to movie info

--- src/views/movies.jsx
+++ src/views/movies.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import { Container } from '@material-ui/core';
+import { Switch, Route } from 'react-router-dom';
+
 import { Header } from '../components/header/header';
 import { MoviesList } from '../components/movies-list/movies-list';
+import { Movie } from '../components/movie/movie'
 
 export const Movies = ({classes}) => {
     
     return (
         <Container component='div'>
             <Header classes={classes}/>
-            <MoviesList></MoviesList>
+            <Switch>
+                <Route exact path='/movies' >
+                    <MoviesList />
+                </Route>
+                <Route path='/movies/:id' >
+                    <Movie />
+                </Route>
+            </Switch>
         </Container>
     )
 }
