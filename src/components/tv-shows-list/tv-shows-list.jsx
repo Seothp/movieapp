7705193b6e0e4cc77ce9a31b0a7af7d7125eda1e
Nikolay Geldyev <nikoejento@gmail.com>
2020-01-31 16:47:44 +0300

    create main functional

--- src/components/tv-shows-list/tv-shows-list.jsx
+++ src/components/tv-shows-list/tv-shows-list.jsx
@@ -4,6 +4,146 @@ import { Card, CardMedia, Grid, Typography, Button, Box } from '@material-ui/cor
 import { makeStyles  } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom'
 
-export const TvShowList = () => {
 
+const useStyles = makeStyles({
+    card: {
+        boxSizing: 'border-box',
+        width: 280,
+        padding: 24,
+        flexGrow: 0,
+        flexShrink: 0,
+    },
+    media: {
+        width: 200,
+        height: 300,
+        marginBottom: 8,
+    },
+    movieTitle: {
+        marginBottom: 16,
+    },
+    inlineSubtitle: {
+        marginRight: 8,
+    }, 
+    buttonBox: {
+        display: 'flex',
+        justifyContent: 'center',
+        marginBottom: 32,
+    },
+});
+
+const cardLinkStyles = {
+    display: 'flex',
+    boxSizing: 'border-box',
+    width: '280px',
+    textDecoration: 'none',
+    flexShrink: 0,
+    marginBottom: 24,
+}
+   
+
+export const TvShowList = () => {
+    const [ list, setList ] = useState([]);
+    const [ page, setPage ] = useState(1);
+
+    const classes = useStyles()
+
+    useEffect(() => {
+        fetchMovies()
+    }, [])
+
+    useEffect(() => {
+        fetchMovies()
+        scrollToTop()
+    }, [page])
+    const fetchMovies = () => {
+        fetch(`${TMD_URL}${DISCOVER_PATH}tv?${TMD_API_KEY}&page=${page}`)
+            .then(res => res.json())
+            .then(result => {
+                setList(result.results);
+            })
+    }
+    const nextPage = () => {
+        if (page < 500) {
+            setPage(page + 1)
+        }
+    }
+    const backPage = () => {
+        if (page > 1) {
+            setPage(page - 1)
+        }
+    }
+    const scrollToTop = () => {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    }
+
+    return (
+        <div className="tv-shows">
+            <Typography 
+            variant='subtitle2' 
+            component='span' 
+            style={{'margin': '16px', 'display': 'inline-block'}}>
+                Current page: {page}
+            </Typography>
+            <Button 
+            onClick={backPage} 
+            variant='contained' 
+            color='primary' 
+            style={{'marginRight': '16px'}}>
+                back
+            </Button>
+            <Button 
+            onClick={nextPage} 
+            variant='contained' 
+            color='primary'>
+                next
+            </Button>
+            <Grid 
+            className='movie-list'
+            container
+            direction="row"
+            justify="space-around"
+            alignItems="stretch"
+            >
+                {list.map((item) => (
+                    <Link to={`/tv/${item.id}` }
+                    style={cardLinkStyles}
+                    key={item.id}>
+                        <Card 
+                        className={classes.card}>
+                            {item.poster_path && 
+                                <CardMedia 
+                                image={`${TMD_IMG_URL}w300${item.poster_path}`} 
+                                title="poster"
+                                className={classes.media}/>
+                            }
+                            <Typography variant='h5' component='h3' className={classes.movieTitle}>
+                                {item.name}
+                            </Typography>
+                            <Typography variant='subtitle2' component='span' className={classes.inlineSubtitle}>
+                                Votes:
+                            </Typography>
+                            <Typography variant='body2' component='span'>
+                                {item.vote_average}
+                            </Typography>
+                            <Typography variant='subtitle2' component='p'>
+                                Discription:
+                            </Typography>
+                            <Typography variant='body2' component='p'>
+                                {item.overview}
+                            </Typography>
+                        </Card>
+                    </Link>
+                ))}
+            </Grid>
+            <Box className={classes.buttonBox}>
+                <Button onClick={backPage} variant='contained' color='primary' style={{'marginRight': '32px'}}>
+                    back
+                </Button>
+                <Button onClick={nextPage} variant='contained' color='primary'>
+                    next
+                </Button>
+            </Box>
+        </div>
+    )
 }
