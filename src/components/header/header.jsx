f1fc5d9971c713957d27c54351e0e2f36c2b8aed
Nikolay Geldyev <nikoejento@gmail.com>
2020-02-04 20:21:41 +0300

    edit styles, add methods and searchValue state

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom'
-import { Box, TextField, Button } from '@material-ui/core'
+import { Box, TextField, Button, MenuItem } from '@material-ui/core'
 import { makeStyles  } from '@material-ui/core/styles';
 
 import { SearchedItems } from '../searched-items/searched-items'
 
+const areas = [
+    { value: 'movies' },
+    { value: 'tv' }
+]
+
+
 const useStyles = makeStyles({
     header: {
         marginBottom: 24,
+        position: 'relative'
     },
     ml2: {
         marginLeft: 16,
     }
 })
 export const Header = () => {
-    const classes = useStyles()
+    let [ searchValue, setSearchValue] = useState('');
+    let [ searchArea, setSearchArea ] = useState('');
+
+    const classes = useStyles();
+
+    const handleInputChange = (e) => {
+        setSearchValue(e.target.value);
+    }
+    
+    const handleSelectChange = () => {
+
+    }
     return (
         <Box component='header' className={`header ${classes.header}`}>
             <Box component='nav' m={2}>
@@ -31,10 +49,17 @@ export const Header = () => {
                 </ul>
             </Box>
             <Box className="search-menu" m={2}>
-                <TextField type='text' className='search-menu__input'/>
+                <TextField select onChange={handleSelectChange} >
+                    {areas.map(item => (
+                        <MenuItem value={item.value} key={item.value}>
+                            {item.value}
+                        </MenuItem>
+                    ))}
+                </TextField>
+                <TextField type='text' className='search-menu__input' value={searchValue} onChange={handleInputChange}/>
                 <Button variant='contained' color='primary' size='small' className={classes.ml2}>Search</Button>
             </Box >
-            <SearchedItems />
+            <SearchedItems searchValue={searchValue}/>
         </Box>
     )
 }
