84cdedd89d329c6e6c878e6cbce839fcfe752359
Nikolay Geldyev <nikoejento@gmail.com>
2020-02-04 20:32:12 +0300

    create components

---
+++ src/components/movies-search-list/movies-search-list.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const MoviesSearchList = () => {
+
+
+    return (
+        <div className="movies-search-list">
+            just for test ( movies )
+        </div>
+    )
+}
---
+++ src/components/tv-shows-search-list/tv-shows-search-list.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const TvShowsSearchList = () => {
+
+
+    return (
+        <div className="tv-shows-search-list">
+            just for test ( tvs )
+        </div>
+    )
+}
