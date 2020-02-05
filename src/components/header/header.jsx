25480c943436bc15d65a64a76eb14b57b4def29c
Nikolay Geldyev <nikoejento@gmail.com>
2020-02-05 18:15:39 +0300

    edit styles, add label

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -6,8 +6,8 @@ import { makeStyles  } from '@material-ui/core/styles';
 import { SearchedItems } from '../searched-items/searched-items'
 
 const areas = [
-    { value: 'movies' },
-    { value: 'tv' }
+    { value: 'Movies' },
+    { value: 'Tv' }
 ]
 
 
@@ -17,6 +17,16 @@ const useStyles = makeStyles({
         position: 'relative'
     },
     ml2: {
+        marginLeft: 16,
+    },
+    mt2: {
+        marginTop: 16,
+    },
+    select: {
+        width: 100,
+    },
+    button: {
+        marginTop: 16,
         marginLeft: 16,
     }
 })
@@ -30,8 +40,8 @@ export const Header = () => {
         setSearchValue(e.target.value);
     }
     
-    const handleSelectChange = () => {
-
+    const handleSelectChange = (e) => {
+        setSearchArea(e.target.value)
     }
     return (
         <Box component='header' className={`header ${classes.header}`}>
@@ -49,17 +59,20 @@ export const Header = () => {
                 </ul>
             </Box>
             <Box className="search-menu" m={2}>
-                <TextField select onChange={handleSelectChange} >
+                <TextField select onChange={handleSelectChange} label="Area" value={searchArea} className={classes.select}>
                     {areas.map(item => (
                         <MenuItem value={item.value} key={item.value}>
                             {item.value}
                         </MenuItem>
                     ))}
                 </TextField>
-                <TextField type='text' className='search-menu__input' value={searchValue} onChange={handleInputChange}/>
-                <Button variant='contained' color='primary' size='small' className={classes.ml2}>Search</Button>
+                <TextField type='text' 
+                    className={classes.mt2}
+                    value={searchValue} 
+                    onChange={handleInputChange}/>
+                <Button variant='contained' color='primary' size='small' className={classes.button}>Search</Button>
             </Box >
-            <SearchedItems searchValue={searchValue}/>
+            <SearchedItems searchValue={searchValue} searchArea={searchArea} />
         </Box>
     )
 }
