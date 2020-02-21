b4ad4048ff45c3d25ce404c369fe173e7aed1d57
Nikolay Geldyev <nikoejento@gmail.com>
2020-02-21 14:36:55 +0300

    fix eslint issue

--- src/components/movie/movie.jsx
+++ src/components/movie/movie.jsx
@@ -8,17 +8,13 @@ export const Movie = () => {
     const [ movie, setMovie ] = useState({})
     const { id } = useParams();
 
-    const fetchMovie = () => {
+    useEffect(() => {
         fetch(`${TMD_URL}movie/${id}?${TMD_API_KEY}`)
             .then(res => res.json())
             .then(result => {
                 setMovie(result);
             })
-    }
-
-    useEffect(() => {
-        fetchMovie()
-    }, [])
+    }, [id])
 
     return (
         <Box >
