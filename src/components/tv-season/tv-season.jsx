5b830d1a0abca7d72f22618aa96a43638e100c8c
Nikolay Geldyev <nikoejento@gmail.com>
2020-02-22 16:36:33 +0300

    add episodes list

--- src/components/tv-season/tv-season.jsx
+++ src/components/tv-season/tv-season.jsx
@@ -1,9 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
-import { Box, Grid } from '@material-ui/core';
+import { Box, Grid, Card } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
 import { Poster } from '../poster/poster' 
 import { TMD_URL, TMD_API_KEY } from '../../constants'
+
+const cardLinkStyles = {
+    display: 'flex',
+    boxSizing: 'border-box',
+    width: '140px',
+    textDecoration: 'none',
+    flexShrink: 0,
+    marginBottom: 24,
+}
+
+const useStyles = makeStyles({
+    card: {
+        boxSizing: 'border-box',
+        width: '100%',
+        padding: 8,
+        flexGrow: 0,
+        flexShrink: 0,
+        display: 'flex',
+        flexDirection: 'column',
+    }
+});
 
 export const TvSeason = () => {
     let [ tvSeason, seTvSeason ] = useState({})
@@ -17,21 +39,44 @@ export const TvSeason = () => {
             })
     }, [id, season_number])
 
+    const classes = useStyles()
+
     return (
         <Box className="tv-season">
             {tvSeason && 
-                <Grid container >
-                    <Poster posterPath={tvSeason.poster_path}/>
-                <Box className="movie-text-info" style={{maxWidth: 900, marginLeft: 16}}>
-                    <h3 className="tv-season-name">
-                        {tvSeason.name}
-                    </h3>
-                    <div className="tv-season-discr">
-                        <b>Discription:</b>
-                        <p>{tvSeason.overview? tvSeason.overview: 'Not found'}</p>
-                    </div>
-                </Box>
-            </Grid>
+                <>
+                    <Grid container >
+                        <Poster posterPath={tvSeason.poster_path}/>
+                        <Box className="movie-text-info" style={{maxWidth: 900, marginLeft: 16}}>
+                            <h3 className="tv-season-name">
+                                {tvSeason.name}
+                            </h3>
+                            <div className="tv-season-discr">
+                                <b>Discription:</b>
+                                <p>{tvSeason.overview? tvSeason.overview: 'Not found'}</p>
+                            </div>
+                        </Box>
+                    </Grid>
+                    <Grid
+                        container
+                        wrap='wrap'
+                        justify='space-between'
+                        
+                    >
+                        {tvSeason.episodes && tvSeason.episodes.map(episode => (
+                            <Link 
+                                style={cardLinkStyles} 
+                                key={episode.id} 
+                                to={`/tv/${id}/season/${season_number}/episode/${episode.episode_number}`}
+                            >
+                                <Card className={classes.card}>
+                                    <span className="episode-number">Ep: {episode.episode_number}.</span>
+                                    <span className="episode-title">{episode.name}</span>
+                                </Card>
+                            </Link>
+                        ))}
+                    </Grid>
+                </>
             }
         </Box>
     )    
