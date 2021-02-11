e88181d88dc9fe545d7971dcb44ad2b95344e2a5
Nikolay <nikoejento@gmail.com>
2021-02-11 21:03:20 +0300

    Updates

---
+++ precache-manifest.2feedec725929e9aed373318e0602bf2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89d4d18bc29fa96b465e537cae7ca32f",
+    "url": "/movieapp/index.html"
+  },
+  {
+    "revision": "acdc8ada2be464327c57",
+    "url": "/movieapp/static/css/main.c32c9405.chunk.css"
+  },
+  {
+    "revision": "2bd1ad47d9ba8d183585",
+    "url": "/movieapp/static/js/2.0d8f0dba.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/movieapp/static/js/2.0d8f0dba.chunk.js.LICENSE"
+  },
+  {
+    "revision": "acdc8ada2be464327c57",
+    "url": "/movieapp/static/js/main.3c386847.chunk.js"
+  },
+  {
+    "revision": "976c1e32db15493d4ca2",
+    "url": "/movieapp/static/js/runtime-main.53a4848c.js"
+  }
+]);
